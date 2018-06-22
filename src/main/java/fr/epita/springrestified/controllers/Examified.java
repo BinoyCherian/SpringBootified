bb7ede24561ce167d30d154691acb55630e716a1
AbdulKShahid <abdulkshahid@gmail.com>
2018-06-22 10:18:13 +0200

    added cross origins to examified, mcqchoicified, questionified, restified corresponding to angular port no.

--- src/main/java/fr/epita/springrestified/controllers/Examified.java
+++ src/main/java/fr/epita/springrestified/controllers/Examified.java
@@ -1,10 +1,12 @@
 package fr.epita.springrestified.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/restified/exam")
+@CrossOrigin(origins = "http://localhost:4200")
 public class Examified {
 
 }
--- src/main/java/fr/epita/springrestified/controllers/MCQChoicified.java
+++ src/main/java/fr/epita/springrestified/controllers/MCQChoicified.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ import fr.epita.springrestified.services.MCQService;
  */
 @RestController
 @RequestMapping(value="/restified/mcqchoice")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MCQChoicified {
 	
 	/** The mcq service */
